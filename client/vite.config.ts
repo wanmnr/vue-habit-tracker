8b247a1c0d71e6080638b1a1f1d198e0da1b414e
Wan <158989003+ridhwanmnr@users.noreply.github.com>
2024-11-12 23:26:16 +0800

    chore(vite.config.ts): configure Ant Design with unplugin-vue-components
    - Added `unplugin-vue-components` plugin with `AntDesignVueResolver` for automatic component imports, removing the need for manual Ant Design component imports.
    - This configuration simplifies setup and improves maintainability of Ant Design components within the project.

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,12 +2,21 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { VitePWA } from 'vite-plugin-pwa';
 import viteImagemin from 'vite-plugin-imagemin';
+import Components from 'unplugin-vue-components/vite'
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    Components({
+      resolvers: [
+        AntDesignVueResolver({
+          importStyle: false, 
+        }),
+      ],
+    }),
     VitePWA({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
